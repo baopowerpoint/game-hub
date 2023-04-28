877c901028733dd27a27353dea7b2c48e7648173
baopowerpoint <baopowerpoint@gmail.com>
2023-04-28 16:54:15 +0700

    displaying platform icons

---
+++ src/components/PlatformIconList.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+  FaPlaystation,
+  FaWindows,
+} from "react-icons/fa";
+import { SiNintendo } from "react-icons/si";
+import { MdPhoneIphone } from "react-icons/md";
+import { BsGlobe } from "react-icons/bs";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+
+const PlatformIconList = ({ platforms }) => {
+  const iconMap = {
+    pc: FaWindows,
+    playStation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+  return (
+    <HStack mt="10px">
+      {platforms &&
+        platforms.map((platform) => (
+          <Icon color="gray.500" as={iconMap[platform.slug]} />
+        ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
