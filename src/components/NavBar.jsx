20ed36bc624b452a7bb3d767f5d49e543eaadd53
baopowerpoint <baopowerpoint@gmail.com>
2023-04-28 15:33:30 +0700

    build color mode switch

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/learn-docker.jpg";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justify="space-between" padding="10px">
       <Image m="10px" src={logo} boxSize="60px" />
-      <Text fontWeight="semibold">NavBar</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
