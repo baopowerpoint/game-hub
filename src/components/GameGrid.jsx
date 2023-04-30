db9b65147ae40fe553a4d625354d7303ec1a9a3c
baopowerpoint <baopowerpoint@gmail.com>
2023-05-01 01:23:39 +0700

    refactor game grid

--- src/components/GameGrid.jsx
+++ src/components/GameGrid.jsx
@@ -6,20 +6,17 @@ import GameCardSkeleton from "./GameCardSkeleton";
 const GameGrid = ({ gameQuery }) => {
   const { data: games, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
-
+  if (error) return <Text>{error}</Text>;
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        padding={"10px"}
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing={6}
-      >
-        {isLoading &&
-          skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
-        {games && games.map((game) => <GameCard key={game.id} game={game} />)}
-      </SimpleGrid>
-    </>
+    <SimpleGrid
+      padding={"10px"}
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      spacing={6}
+    >
+      {isLoading &&
+        skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
+      {games && games.map((game) => <GameCard key={game.id} game={game} />)}
+    </SimpleGrid>
   );
 };
 
